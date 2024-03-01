dfe9b5931a3d9bab95b08c260979a41e859ebac9
Soe Wai Yan Aung <soewaiyanag@gmail.com>
2024-03-01 12:33:59 +0700

    add active state to BoardMenu

--- src/components/BoardMenu.vue
+++ src/components/BoardMenu.vue
@@ -1,30 +1,40 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import BoardIcon from './BoardIcon.vue';
+
+const boards = ref([]);
+const activeBoard = ref(0);
+
+async function fetchBoards() {
+  const response = await fetch('/data.json');
+  const data = await response.json();
+  boards.value = data?.boards;
+}
+
+onMounted(fetchBoards);
 </script>
 
 <template>
   <div class="text-nowrap pt-6">
     <h2 class="mb-4 text-base font-normal uppercase text-battleship-grey">
-      All Boards (3)
+      All Boards ({{ boards.length }})
     </h2>
     <ul>
       <li
-        class="-ml-12 flex items-center gap-3 rounded-r-3xl bg-purple-heart py-4 pl-12 text-white"
+        v-for="({ name }, index) in boards"
+        class="cursor-pointer font-semibold transition-colors"
+        :class="{
+          '-ml-12 flex items-center gap-3 rounded-r-3xl bg-purple-heart py-4 pl-12 text-white':
+            activeBoard === index,
+          '-ml-12 flex items-center gap-3 rounded-r-3xl py-4 pl-12 text-battleship-grey hover:bg-purple-heart/10 hover:text-purple-heart':
+            activeBoard !== index,
+        }"
+        @click="activeBoard = index"
       >
-        <BoardIcon :fill="'white'" />
-        Platform Launch
-      </li>
-      <li
-        class="-ml-12 flex items-center gap-3 rounded-r-3xl py-4 pl-12 text-battleship-grey"
-      >
-        <BoardIcon :fill="'var(--battleship-grey)'" />
-        Platform Launch
-      </li>
-      <li
-        class="-ml-12 flex items-center gap-3 rounded-r-3xl py-4 pl-12 text-battleship-grey"
-      >
-        <BoardIcon :fill="'var(--battleship-grey)'" />
-        Platform Launch
+        <BoardIcon
+          :fill="activeBoard === index ? 'white' : 'var(--battleship-grey)'"
+        />
+        {{ name }}
       </li>
     </ul>
   </div>
